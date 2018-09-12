60ff194b14312944ced76283492face397c3cea1
wang.guangbin <wanggb@nroad.com.cn>
2018-09-12 20:36:23 +0800

    add counting_sort_ext

--- counting_sort.py
+++ counting_sort.py
@@ -22,13 +22,36 @@ def counting_sort(A,B,k):
         C[v] = C[v] - 1
 
 
+def counting_sort_ext(A,B):
+    B = [0] * len(A)
+
+    min = A[0]
+    max = A[0]
+    for v in A:
+        if v > max:
+            max = v
+        elif v < min:
+            min = v
+
+    C = [0] * (max - min + 1)
+    A = [v - min for v in A]
+
+    counting_sort(A,B,max - min + 1)
+    B = [v + min for v in A]
+
+
 vals = [1,3,4,2,7]
 result = [0] * len(vals)
 counting_sort(vals,result,8)
 print(result)
 
+counting_sort_ext(vals,result)
+print(result)
 
 vals = [6,3,4,2,7,34,51,2,8,5,4,34]
 result = [0] * len(vals)
 counting_sort(vals,result,52)
+print(result)
+
+counting_sort_ext(vals,result)
 print(result)
