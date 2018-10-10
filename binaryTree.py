2157b5873ac714e8e5b057cea0d764ec8b40ab3f
wang.guangbin <wanggb@nroad.com.cn>
2018-10-10 20:50:08 +0800

    二叉树 深度和广度遍历

---
+++ binaryTree.py
@@ -0,0 +1,92 @@
+
+
+class BTreeItem():
+
+    def __init__(self,dat):
+        self.dat = dat
+        self.left = None
+        self.right = None
+        self.p = None
+    
+    def d_show(self):
+        print(self.dat)
+
+        l = self.left
+        if l != None:
+            l.show()
+        
+        r = self.right
+        if r != None:
+            r.show()
+    
+    def show(self):
+        print(self.dat)
+
+    def search(self,kval):
+        if self.dat == kval:
+            return self
+        else:
+            l = self.left
+            if l != None:
+                val = l.search(kval)
+                if val != None:
+                    return val
+            
+            r = self.right
+            if r != None:
+                val = r.search(kval)
+                if val != None:
+                    return val
+
+        return None
+
+class BinaryTree():
+
+    def __init__(self):
+        self.root = None
+
+    def d_show(self):
+        node = self.root
+        if node != None:
+            node.d_show()
+
+    def w_show(self):
+        cur = list()
+        cur.append(self.root)
+        
+        while len(cur) > 0:
+            childs = list()
+            for node in cur:
+                if node != None:
+                    node.show()
+                    childs.append(node.left)
+                    childs.append(node.right)
+            
+            cur = childs
+
+    def d_show_2(self):
+        nodes = list()
+        if self.root != None:
+            nodes.append(self.root)
+
+        while len(nodes) > 0:
+            node = nodes[-1]
+            nodes.pop()
+
+            if node.right != None:
+                nodes.append(node.right)
+            
+            node.show()
+
+            if node.left != None:
+                nodes.append(node.left)
+
+    def addChild(self,node,child):
+        if node == None:
+            self.root = child
+
+        if child.p == None:
+            child.p = node
+            return node
+        else:
+            return None
